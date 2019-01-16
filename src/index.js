6dc0ab3d1046f94e0a72a1ef96c34dae23552aab
Jason Bland <jtbland@gmail.com>
2019-01-15 22:20:47 -0800

    add redux boilerplate to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { Provider } from 'react-redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import ReduxPromise from 'redux-promise';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+import App from './components/App';
+import reducers from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(ReduxThunk, ReduxPromise)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
