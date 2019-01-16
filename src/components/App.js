8af744b9a7788db8e180c48ace4a34fb1a1facef
Jason Bland <jtbland@gmail.com>
2019-01-15 21:48:51 -0800

    add custom boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import 'semantic-ui-css/semantic.min.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>App</div>
+      </div>
+    );
+  }
+}
+
+export default App;
