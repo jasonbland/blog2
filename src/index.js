e2b800e6f1c6f0504c7cc4b1b8d182d5cb224af7
Jason Bland <jtbland@gmail.com>
2019-01-16 23:15:07 -0800

    replace app with postsindex and add browserrouter

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, compose, createStore } from 'redux';
+import { BrowserRouter, Route, History } from 'react-router-dom';
 import ReduxThunk from 'redux-thunk';
 import ReduxPromise from 'redux-promise';
 
-import App from './components/App';
 import reducers from './reducers';
+import PostsIndex from './components/PostsIndex';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(ReduxThunk, ReduxPromise)));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.querySelector('#root')
 );
