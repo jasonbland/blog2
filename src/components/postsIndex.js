acb7ce731303e2de02bee2a93ba1b8de1d7fceb0
Jason Bland <jtbland@gmail.com>
2019-01-17 22:58:48 -0800

    add redux to posts index comp

--- src/components/postsIndex.js
+++ src/components/postsIndex.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
   render() {
@@ -6,4 +8,7 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+export default connect(
+  null,
+  { fetchPosts }
+)(PostsIndex);
