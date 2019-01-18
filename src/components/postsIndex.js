923de43ef1a87f35b310dec23c0052e2fdb157c6
Jason Bland <jtbland@gmail.com>
2019-01-17 23:05:55 -0800

    add mapstatetoprops and fetchposts to componentdidmount

--- src/components/postsIndex.js
+++ src/components/postsIndex.js
@@ -3,12 +3,23 @@ import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return <div>Posts Index</div>;
   }
 }
 
+const mapStateToProps = ({ posts }) => {
+  console.log(posts);
+  return {
+    posts
+  };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchPosts }
 )(PostsIndex);
