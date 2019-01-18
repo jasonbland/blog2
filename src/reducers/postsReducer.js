7635c563670affed28834d3b6e3074a6376c82a0
Jason Bland <jtbland@gmail.com>
2019-01-17 22:54:31 -0800

    add lodash and posts reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,4 +1,11 @@
-export default (state, action) => {
+import _ from 'lodash';
+import { FETCH_POSTS } from '../actions';
+
+export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_POSTS:
+      return _.mapKeys(action.payload.data, 'id');
+    default:
+      return state;
   }
 };
